eac01bc2b6108ad286d62ebd8d5c235fe9b5e21a
Joe Trinidad <joetrinidad@Joes-Air.fios-router.home>
2022-07-10 14:14:50 -0400

    adding deploy scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t jet090892/multi-client:latest -t jet090892/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t jet090892/multi-server:latest -t jet090892/multi-server:$SHA -f ./server/Dockerfile ./server
+dcoker build -t jet090892/multi-worker:latest -t jet090892/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push jet090892/multi-client:latest
+docker push jet090892/multi-server:latest
+docker push jet090892/multi-worker:latest
+
+docker push jet090892/multi-client:$SHA
+docker push jet090892/multi-server:$SHA
+docker push jet090892/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployment/server-deployment server=jet090892/multi-server:$SHA
+kubectl set image deployment/client-deployment client=jet090892/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=jet090892/multi-worker:$SHA
